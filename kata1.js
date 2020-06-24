ef557c8cd63bfcaec6c44fd3db45ad7b4357e536
Angel Tsang <blacitea@gmail.com>
2020-06-24 21:08:35 +0000

    use of nested for-loop

--- kata1.js
+++ kata1.js
@@ -7,7 +7,7 @@ const sumLargestNumbers = function(data) {
   for (let i = 0; i< data.length; i++) {
     
 //  To verify if the for loop is working or not
-    console.log(i);
+//  console.log(`i = ${i}`);
 
 //  If the data in front is larger, it swap spot with the data at back
     if (data[i] > data[i + 1]) {
@@ -15,51 +15,26 @@ const sumLargestNumbers = function(data) {
       temp = data[i];
       data[i] = data[i + 1];
       data[i+1] = temp;
-
-//    To verify if the updated array has correct order 
-      console.log(data);
-
-//    After initial swap, if i is smaller than i - 1, we need to keep swapping
-      if (data[i - 1] > data[i]) {
-      let temp;
-      temp = data[i - 1];
-      data[i - 1] = data[i];
-      data[i] = temp;
-      
-//    After second swap, if i - 1 is smaller than i - 2, we need to keep swapping
-        if (data[i - 2] > data[i - 1]) {
-          let temp;
-          temp = data[i - 2];
-          data[i - 2] = data[i - 1];
-          data[i - 1] = temp;
-
-          // Loop again ....
-          if (data[i - 3] > data[i - 2]) {
-            let temp;
-            temp = data[i - 3];
-            data[i - 3] = data[i - 2];
-            data[i - 2] = temp;
-
-                      // Loop again ....
-          if (data[i - 4] > data[i - 3]) {
-            let temp;
-            temp = data[i - 4];
-            data[i - 4] = data[i - 3];
-            data[i - 3] = temp;
-          }
-          }
-        }
-      };
-
-//    To verify if swap happened
-      console.log(data);
-
     };
+    let b = i;
+    let a = b - 1;
+//  After initial swap, keep compair with value at index i - 1 until smallest number at front
+    for ( let c = i; c >= 0; c --) {
+      console.log(`b = ${b}`);
+      console.log(`a = ${a}`);
+      if (data[a] > data[b]) {
+      let temp;
+      temp = data[a];
+      data[a] = data[b];
+      data[b] = temp;
+      b --;
+      a --;
+      }
+    }
   };
-  let arrLength = data.length
+//  Calculate the sum of the largest two number i.e. last two of the array
+  let arrLength = data.length;
   let sum = data[arrLength - 1] + data[arrLength - 2];
+//  Return the result
   return sum;
 }
-
-console.log(sumLargestNumbers([10, 4, 34, 6, 92, 2]));
-
