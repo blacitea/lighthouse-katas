6c6a8f58afd98872b11263838b2ba7afef3c3289
Angel Tsang <blacitea@gmail.com>
2020-06-27 04:44:47 +0000

    updated comments

--- caseMaker.js
+++ caseMaker.js
@@ -1,15 +1,18 @@
 const camelCase = function(input) {
-  // input is a string
-  // return string as camelCase (no whitespace, uppercase for every first letter from second word)
-  // Step 1, loop through the array
-  // Step 2, use String.search(/\s/g) to find index of white space
-  // Step 3, use String.replace to remove (n & n+1) with (n).toUpperCase
-  let camel = '';
+// input is a string
+// return string as camelCase (no whitespace, uppercase for every first letter from second word)
+
+// New variable to store the updated string 
+   let camel = '';
+// Looping the input string
   for( let i = 0; i < input.length; i ++ ) {
     if( input[i] === ' ') {
+// If found whitespace, replace with uppercase of the next character
       camel += input[i + 1].toUpperCase();
+// Extra i + 1 to by pass the index for next lop
       i += 1;
     } else {
+// When no whitespace, just concatenate 
       camel += input[i];
     }
   }
